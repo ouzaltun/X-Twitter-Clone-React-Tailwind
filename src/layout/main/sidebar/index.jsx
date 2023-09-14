94789b345d2c28f4e39450a4e0846eecd76c5a2a
Oğuzhan Altun <altunnoguzhan@gmail.com>
2023-09-15 01:51:37 +0300

    layout padding eksik kalmasın
    layout padding eksik kalmasın

--- src/layout/main/sidebar/index.jsx
+++ src/layout/main/sidebar/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Sidebar() {
-  return <div className="w-[275px] min-h-screen ">Sidebar</div>;
+  return <div className="w-[275px] min-h-screen px-2">Sidebar</div>;
 }
 
 export default Sidebar;
